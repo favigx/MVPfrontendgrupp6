5ee6fccd5cfbc951d03e6c0e4d3710f83544cc96
Viktor Falkesjö <viktorfalkesjo@gmail.com>
2024-03-11 11:34:19 +0100

    Connectiontobackend

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("Hello script");
+
+let productList = document.getElementById("productList");
+
+printProducts();
+
+function printProducts(){
+    fetch("http://localhost:8080/api/product/")
+    .then(res => res.json())
+    .then(data =>{
+        data.forEach(product =>{
+            let li = document.createElement("li");
+            li.innerText = product.productName;
+            
+            productList.appendChild(li);
+        })
+    })
+}
+
